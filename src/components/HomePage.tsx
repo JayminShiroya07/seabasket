cd8797aceca16fe03f38fb809d54bdcbd1c5e43e
jaymin shiroya <jaymeenshiroya399@gmail.com>
2025-03-26 16:55:37 +0530

    ui : trending section added with shuffled items

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,21 +1,28 @@
-import shoes from "../assets/homePageImage/shoes.png";
-import watch from "../assets/homePageImage/watch.png";
-import laptop from "../assets/homePageImage/laptop.png";
-import mobile from "../assets/homePageImage/mobile.png";
-import woman from "../assets/homePageImage/woman-clothes.png";
-
 import { AnimatePresence, motion } from "framer-motion";
 import { useState, useEffect } from "react";
 import { categories, Category } from "../data/category";
+import ProductItem from "./ProductItem";
+import { images as categoryImage, productImages } from "../data/images";
 
 export default function HomePage() {
-  const images = [shoes, watch, laptop, mobile, woman];
+  const images = categoryImage;
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [fatchedCategories, setFatchedCategories] = useState<Category[]>([]);
+  const [trandingProducts, setTrandingProducts] = useState<String[]>([]);
+
+  function shuffleArray<T>(array: T[]): T[] {
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
+  }
 
   useEffect(() => {
     setFatchedCategories(categories);
     const interval = setInterval(() => {
+      setTrandingProducts(shuffleArray(productImages).slice(0, 5));
       setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
     }, 5000);
     return () => clearInterval(interval);
@@ -25,7 +32,7 @@ export default function HomePage() {
     <>
       <section className="home w-full md:h-[calc(100vh-400px)] flex flex-col p-10 md:flex-row-reverse">
         <div className="md:w-1/2 w-full p-3 min-h-2/4 flex justify-center items-center">
-          <div className="w-full h-full flex justify-start gap-2 p-5">
+          <div className="w-full h-full flex justify-center gap-2 p-5">
             <AnimatePresence mode="wait">
               <motion.img
                 key={currentImageIndex}
@@ -45,7 +52,7 @@ export default function HomePage() {
                   scale: 0,
                 }}
                 transition={{ duration: 0.3, ease: "easeInOut" }}
-                src={images[currentImageIndex]}
+                src={categoryImage[currentImageIndex]}
                 alt=""
                 className="drop-shadow-2xl bg-cover"
               />
@@ -57,7 +64,9 @@ export default function HomePage() {
             <h1 className="text-2xl md:text-5xl font-bold">New Collection</h1>
             <p className="md:text-2xl mt-4">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi,
-              quidem?
+              quidem? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Unde eos illo provident esse voluptatibus possimus, iure ducimus
+              consequatur explicabo quaerat!
             </p>
             <button className="btn-primary mt-4">Shop Now</button>
           </div>
@@ -65,7 +74,7 @@ export default function HomePage() {
       </section>
 
       {/* category section */}
-      <h2 className="text-center p-3 text-4xl font-bold">Our Categories</h2>
+      <h2 className="text-center p-3 text-4xl font-bold">Categories</h2>
       <section className="w-full p-6 overflow-hidden">
         <motion.div
           className="flex gap-6 w-max"
@@ -98,6 +107,48 @@ export default function HomePage() {
             )
           )}
         </motion.div>
+      </section>
+
+      {/* tranding section */}
+      <h2 className="text-center p-3 mt-12 text-4xl font-bold">
+        Trending Products
+      </h2>
+      <section className="w-full mb-30 p-5">
+        <div className="flex md:flex-row flex-col gap-5">
+          {trandingProducts.map((img, index) => (
+            <ProductItem
+              key={index}
+              className="rounded-md border-2 w-full min-h-fit overflow-hidden md:w-1/4"
+            >
+              <div className="h-1/6 w-full  bg-primary items-center p-2 flex justify-between">
+                <ProductItem.Title className="text-2xl text-center text-secondary font-bold">
+                  Title
+                </ProductItem.Title>
+                <ProductItem.Button
+                  className="w-12 h-12 flex items-center justify-center text-white text-2xl rounded-full hover:bg-red-600 hover:text-white transition"
+                  icon="fas fa-heart"
+                />
+              </div>
+              <div className="h-5/6 w-full overflow-hidden flex justify-center items-center">
+                <ProductItem.Image
+                    image={img.toString()}
+                    className="h-fit w-full object-contain"
+                    />
+              </div>
+              <div className="h-1/6 border-t-2 px-3 py-1 flex justify-between items-center">
+                <ProductItem.Price className="font-bold text-2xl">
+                  123
+                </ProductItem.Price>
+                <ProductItem.Button
+                  className="p-2 rounded outline-1 outline-blue-400 hover:bg-[#1E40AF] hover:text-white"
+                  type="buy"
+                >
+                  But Now
+                </ProductItem.Button>
+              </div>
+            </ProductItem>
+          ))}
+        </div>
       </section>
     </>
   );
---
+++ src/data/images.ts
@@ -0,0 +1,27 @@
+import shoes from "../assets/homePageImage/shoes.png";
+import watch from "../assets/homePageImage/watch.png";
+import laptop from "../assets/homePageImage/laptop.png";
+import mobile from "../assets/homePageImage/mobile.png";
+import woman from "../assets/homePageImage/woman-clothes.png";
+
+export const images = [shoes, watch, laptop, mobile, woman];
+
+
+import camera from '../assets/homePageImage/products/p_1.jpeg';
+import projector from '../assets/homePageImage/products/p_2.jpeg';
+import cube from '../assets/homePageImage/products/p_3.jpeg';
+import toy from '../assets/homePageImage/products/p_4.jpeg';
+import smartWatch from '../assets/homePageImage/products/p_5.jpeg';
+
+
+export const productImages = [
+    camera,
+    projector,
+    cube,
+    toy,
+    smartWatch,
+    shoes,
+    laptop,
+    mobile,
+    woman
+]
