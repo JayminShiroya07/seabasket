06384cfd7864af2defa9b6b6edecd6350e5bbe3a
jaymin shiroya <jaymeenshiroya399@gmail.com>
2025-03-26 12:50:32 +0530

    ui : category list added in home section

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -6,12 +6,15 @@ import woman from "../assets/homePageImage/woman-clothes.png";
 
 import { AnimatePresence, motion } from "framer-motion";
 import { useState, useEffect } from "react";
+import { categories, Category } from "../data/category";
 
 export default function HomePage() {
   const images = [shoes, watch, laptop, mobile, woman];
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [fatchedCategories, setFatchedCategories] = useState<Category[]>([]);
 
   useEffect(() => {
+    setFatchedCategories(categories);
     const interval = setInterval(() => {
       setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
     }, 5000);
@@ -20,8 +23,7 @@ export default function HomePage() {
 
   return (
     <>
-      {/* home section */}
-      <section className="home w-full h-[calc(100vh-100px)] flex flex-col p-10 md:flex-row-reverse">
+      <section className="home w-full md:h-[calc(100vh-400px)] flex flex-col p-10 md:flex-row-reverse">
         <div className="md:w-1/2 w-full p-3 min-h-2/4 flex justify-center items-center">
           <div className="w-full h-full flex justify-start gap-2 p-5">
             <AnimatePresence mode="wait">
@@ -48,9 +50,6 @@ export default function HomePage() {
                 className="drop-shadow-2xl bg-cover"
               />
             </AnimatePresence>
-            {/* // <img src={images[currentImageIndex-1]}  alt="" className='absolute opacity-5'/>
-                // <img src={images[currentImageIndex]}  alt="" className='relative bg-amber-200' />
-                // <img src={images[currentImageIndex+1]}  alt="" className='absolute opacity-5'/> */}
           </div>
         </div>
         <div className=" flex flex-col w-full md:w-1/2 justify-center items-end h-full">
@@ -66,8 +65,39 @@ export default function HomePage() {
       </section>
 
       {/* category section */}
-      <section className="w-full bg-amber-50">
-        <div>hello</div>
+      <h2 className="text-center p-3 text-4xl font-bold">Our Categories</h2>
+      <section className="w-full p-6 overflow-hidden">
+        <motion.div
+          className="flex gap-6 w-max"
+          initial={{ x: 0 }}
+          animate={{ x: "-50%" }}
+          transition={{
+            ease: "linear",
+            duration: 35,
+            repeat: Infinity,
+          }}
+        >
+          {[...fatchedCategories, ...fatchedCategories].map(
+            (category, index) => (
+              <div
+                key={index}
+                className="bg-white shadow-lg border-2 rounded-xl overflow-hidden min-w-[250px] max-w-[300px] transform transition duration-300 hover:scale-105"
+              >
+                <div className="w-full h-48 flex items-center justify-center">
+                  <img
+                    src={category.image}
+                    alt={category.title}
+                    className="max-h-full max-w-full object-contain"
+                  />
+                </div>
+
+                <div className="p-4 bg-primary text-white text-center">
+                  <h2 className="text-lg font-semibold">{category.title}</h2>
+                </div>
+              </div>
+            )
+          )}
+        </motion.div>
       </section>
     </>
   );
--- src/data/category.ts
+++ src/data/category.ts
@@ -1,11 +1,25 @@
+import shoes from "../assets/homePageImage/shoes.png";
+import watch from "../assets/homePageImage/watch.png";
+import laptop from "../assets/homePageImage/laptop.png";
+import mobile from "../assets/homePageImage/mobile.png";
+import woman from "../assets/homePageImage/woman-clothes.png";
+
 export interface Category {
     id: number;
     title: string;
+    image : string;
 }
 
 export const categories: Category[] = [
-    { id: 101, title: "Category 1" },
-    { id: 102, title: "Category 2" },
-    { id: 103, title: "Category 3" },
-    { id: 105, title: "Category 3" },
+    { id: 101, title: "Category 1" , image: shoes},
+    { id: 102, title: "Category 2" , image: watch},
+    { id: 103, title: "Category 3" , image: mobile},
+    { id: 104, title: "Category 4" , image: woman},
+    { id: 105, title: "Category 5" , image: laptop},
+    { id: 106, title: "Category 6" , image: shoes},
+    { id: 107, title: "Category 7" , image: watch},
+    { id: 108, title: "Category 8" , image: mobile},
+    { id: 109, title: "Category 9" , image: shoes},
+    { id: 110, title: "Category 10" , image: woman},
+    { id: 111, title: "Category 11" , image: watch},
 ];
