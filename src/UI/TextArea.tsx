9558d55ccf7f73d47b6b304f80e6d75c167f17e0
jaymin shiroya <jaymeenshiroya399@gmail.com>
2025-03-27 19:08:27 +0530

    ui : contact-us component completed

---
+++ src/UI/TextArea.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface ChildNode {
+  children?: React.ReactNode;
+  className?: string;
+  placeHolder?: string;
+  name?: string;
+  label?: string;
+}
+
+const TextArea: React.FC<ChildNode> = (props) => {
+  return (
+    <div className="flex flex-col mt-3">
+      <label htmlFor={props.name} className="font-medium">
+        {props.label}
+      </label>
+      <textarea
+        className={`${props.className} p-2 border-[0.5px] rounded bg-white placeholder:font-mono placeholder:text-gray-500 text-black font-medium`}
+        placeholder={props.placeHolder}
+      />
+    </div>
+  );
+};
+
+export default TextArea;
